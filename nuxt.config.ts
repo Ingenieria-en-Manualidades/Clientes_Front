91bebfb41d79e54dcfb07cf4fbddf73caa1991af
CDQUINONEZ <auxiliar1.tecnologia-op@ienm.com.co>
2024-08-30 15:59:26 -0500

    General-Changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,24 +2,29 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+
   plugins: [
     '~/plugins/vueuse.ts'
   ],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+
   modules: [
     'nuxt-primevue'
   ],
+
   primevue: {
     options: {
       unstyled: true
     },
     importPT: { as: 'Aura', from: '~/presets/aura' }
   },
+
   runtimeConfig: {
     public: {
       cookieOptions: {
@@ -28,4 +33,6 @@ export default defineNuxtConfig({
       },
     },
   },
+
+  compatibilityDate: '2024-08-28',
 });
--- server/api/cookiesRemisiones.post.ts
+++ server/api/cookiesRemisiones.post.ts
@@ -1,19 +1,23 @@
 // Endpoint importado en el composable 'loginApi'.
 export default defineEventHandler(async (event) => {
+    
     // Se toma las variables que se envian desde un fetch.
     const { token, idCliente, usuario } = await readBody(event);
 
     // Creamos las cookies con el método nuxt 'setCookie'.
     setCookie(event, 'token', token, {
         sameSite: 'none',
+        httpOnly: true,
         secure: true
     });
     setCookie(event, 'idCliente', idCliente, {
         sameSite: 'none',
+        httpOnly: true,
         secure: true
     });
     setCookie(event, 'usuario', usuario, {
         sameSite: 'none',
+        httpOnly: true,
         secure: true
     });
 
