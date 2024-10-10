f57746dc1a64e1cee36d32192ef14c1b5d921b8c
CDQUINONEZ <auxiliar1.tecnologia-op@ienm.com.co>
2024-10-10 11:00:44 -0500

    bug-fix

--- middleware/login.global.ts
+++ middleware/login.global.ts
@@ -11,9 +11,13 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
 
     const resultado = await verificarLogin();
     
-    if (!resultado) {
-        if (to.path !== "/") {
-            return navigateTo("/");
-        }
-    }
-})
+// Si no está autenticado, redirigir a la página de login si no estamos ya allí.
+if (!resultado && to.path !== "/") {
+    return navigateTo("/");
+}
+
+// Si está autenticado y está intentando acceder a la página de login, redirigir a una ruta autorizada.
+if (resultado && to.path === "/") {
+    return navigateTo("/dashboard"); // Cambia a la ruta que quieras redirigir después del login.
+}
+});
